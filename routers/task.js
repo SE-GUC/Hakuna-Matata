6db2acf4f51bc0c7908fb73599d07442835bfee1
AbdelrahmanBadr3 <body.badr1510@gmail.com>
2019-03-02 17:37:30 +0200

    Add accepted or reject task and view all tasks and view specifiv task

--- routers/task.js
+++ routers/task.js
@@ -9,3 +9,50 @@ const tasks= require('../arrays/tasks.js');
 const platform= require('../arrays/platform.js');
 const members= require('../arrays/members.js');
 const {Send_Task_Notification} = require("../arrays/Notifications.js");
+
+
+// Update a tasks's state (taskID =>taskId , adminId=> admin who reject or accept the task)
+router.put('/:adminId/edit/:taskID', (req, res) => {
+    const admin_Id=req.params.adminId
+    const task_Id = req.params.taskID 
+    const acceptancy = req.body.state
+    const vartask = tasks.find(vartask => parseInt(vartask.id) === parseInt(task_Id))
+    //console.log(vartask)
+    if(vartask!== undefined){
+        vartask.accepted = acceptancy
+        if(vartask.accepted===true){       
+            const  task1=new task(  vartask.id,vartask.partner_id,vartask.consultancy_agency_id,vartask.member_id,admin_Id,
+                                    vartask.applied_id,vartask.description,vartask.required_skills,
+                                    vartask.monetary_compensation,vartask.deadline,vartask.deadline_for_apply,
+                                    vartask.upload_date,vartask.submission_date,vartask.experience_level,
+                                    vartask.commit_level, vartask.work_cycle,vartask.link_of_task,vartask.user_rate,
+                                    vartask.accepted,vartask.rate,vartask.consulty_needed,vartask.consulty_members)
+            tasks.splice(tasks.indexOf(vartask),1)
+            platform.push(task1.id)
+            tasks.push(task1)
+            var e=Send_Task_Notification(task_Id,vartask.partner_id,"Your task has been accepted");
+        }
+        else{
+            var e=Send_Task_Notification(task_Id,vartask.partner_id,"Your task has been rejected");
+            tasks.splice(tasks.indexOf(vartask),1)
+        }
+         res.send(tasks)
+
+    }
+    else{
+        res.send('there is not such task') 
+    }
+    
+})
+
+// get a specific task      (id =>taskId)
+router.get("/:id" ,(req,res)=>{
+    const task =tasks.find(m=>m.id===parseInt(req.params.id));
+    res.send(task)
+});
+
+//show all tasks
+router.get("/",(req,res)=>{
+    res.send(tasks);
+});
+
