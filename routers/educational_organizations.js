bdba5089686e38bbd82a905b6f5978ae08c6e027
mayarlotfe <mayar.youssef@student.guc.edu.eg>
2019-03-02 19:38:44 +0200

    reserve workshop

--- routers/educational_organizations.js
+++ routers/educational_organizations.js
@@ -29,7 +29,36 @@ router.delete("/:id/delete_educational_organization",(req,res) =>{
 
     res.send({data:educational_organizations});
     
-});   
+}); 
+//reserve workshop for course  
+router.put('/:id/accept_reservation/:room_id',(req,res)=>{
+ 
+    const schema={
+        reserved:Joi.boolean(),
+        reserved_date:Joi.string(),
+        end_of_reservation:Joi.string(),
+        reserved_id: Joi.number().integer()
+    };
+    const result =Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+   
+    const co = coworking_spaces.find(m=>m.id===parseInt(req.params.id));
+    const roooms = co.rooms;
+    const room = roooms.find(m=>m.id===parseInt(req.params.room_id));
+    if(room.reserved===false){
+      room.reserved_id = req.body.reserved_id;
+        room.reserved=req.body.reserved;
+        room.reserved_date=req.body.reserved_date;
+        room.end_of_reservation=req.body.end_of_reservation;
+        res.send(room);
+        return;
+    };
+
+    res.send("this room is not available");
+})
 
 
 
