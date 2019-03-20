a30247a540e1cef8b5e4656152a8447f23f9e878
ahmednightingale <superstar2010z@gmail.com>
2019-03-20 16:37:48 +0200

    Create and Update a partner

--- routers/partner.js
+++ routers/partner.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Joi = require('joi');
+
 
 // We will be connecting using database 
 const partner = require('../models/partner')
@@ -9,6 +11,91 @@ const projects=require('../arrays/projects')
 const  partners = require('../arrays/partners')
 
 const {notificationSummaries} = require("../arrays/Notifications.js");
+
+// create a partner
+router.post('/create',(req,res)=>{
+   
+    const schema={
+        name:Joi.string().required(),
+        information:Joi.string().required(),
+       partners:Joi.array().required(),
+       field_of_work:Joi.string().required(),
+       projects:Joi.array().required(),
+       feedback_form:Joi.string().required()
+
+    };
+    const result =Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const par = new partner(
+        partners.length+1,
+        
+        req.body.name,
+        req.body.information,
+        req.body.partners,
+        req.body.field_of_work,
+        req.body.projects,
+        req.body.feedback_form
+        
+    );
+    partners.push(par);
+    var flag = false;
+    par.partners.forEach(ele => {
+        const element = partners.find(pl=> pl.id === ele)
+   if(element===undefined){
+       res.send("Unfound partner");
+       return;
+   }
+        element.partners.forEach(element2 => {
+        
+        if(element2===par.id){
+            flag=true;
+        }
+    })
+    if(!flag){
+        element.partners.push(par.id)
+    }
+})
+    res.send({data:partners});
+    
+});
+//update a partner
+router.put("/:id/update",(req,res)=>{
+    const schema={
+        name:Joi.string().required(),
+        information:Joi.string().required(),
+       partners:Joi.array().required(),
+       field_of_work:Joi.string().required(),
+       projects:Joi.array().required(),
+       feedback_form:Joi.string().required()
+   
+
+    };
+    const result =Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const par =partners.find(m=>m.id===parseInt(req.params.id));    
+ if(req.body.name!=null){
+       par.name=req.body.name;
+   }if(req.body.information!=null){
+    par.information=req.body.information;
+}if(req.body.partners!=null){
+    par.partners=req.body.partners;
+}if(req.body.field_of_work!=null){
+    par.field_of_work=req.body.field_of_work;
+}if(req.body.projects!=null){
+    par.projects=req.body.projects;
+}if(req.body.feedback_form!=null){
+    par.feedback_form=req.body.feedback_form;
+}
+res.send({data:partners});
+
+});
+
 
 //show my project (id =>partnerId)
 router.get('/:id/show_projects',(req,res)=>{
