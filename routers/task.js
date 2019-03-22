e76c19027d79371b10a86ee193c97fd4762c88c4
abdogareb <abdelrahman.abas@student.guc.edu.eg>
2019-03-23 01:23:55 +0200

    removed require to array tasks

--- routers/task.js
+++ routers/task.js
@@ -6,10 +6,10 @@ const Joi = require('joi');
 const task = require('../models/task.js'); 
 
 //const taskConsulted = require('../arrays/taskConsulted.js'); 
-const tasks= require('../arrays/tasks.js');
-const platform= require('../arrays/platform.js');
-const members= require('../arrays/members.js');
-const {Send_Task_Notification} = require("../arrays/Notifications.js");
+//const tasks= require('../arrays/tasks.js');
+//const platform= require('../arrays/platform.js');
+//const members= require('../arrays/members.js');
+const {Send_Task_Notification} = require('../models/Notification.js');
 
 //create task
 router.post('/create/:id',async (req,res)=>{
