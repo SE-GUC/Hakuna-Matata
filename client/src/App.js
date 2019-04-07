113045d1b01af3a8ca3848455bd8dfb3174264dc
Abdelrahman badr <abdelrahman@Abdelrahmans-MacBook-Pro.local>
2019-04-07 21:45:08 +0200

    React

---
+++ client/src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CardList from './components/CardList';
+import Footer from './components/Layout/Footer';
+import Header from './components/Layout/Header';
+import MemberProfile from './components/Profiles/MemberProfile';
+
+
+class App extends Component {
+  // creatTable() {
+  //   let buttons = []
+  //   for (let i = 0; i < this.state.users.length; i++) {
+  //     if (i % 2 == 0) {
+  //       buttons.push( <Card user={this.state.users[i]} type='female' />)
+  //     } else {
+  //       buttons.push(<Card user={this.state.users[i]} type='male' />)
+  //     }
+
+
+  //   }
+  //   return buttons
+  // }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header></Header>
+          <div className="App-header">
+            {/*this Route will be the main route of the App (Home Page of All)*/}
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <div className="App-list">
+                  <h1 style={{
+                    color: 'black',
+                    textAlign: 'center',
+                  }}> Welcome in Hakuna-Matata Site</h1>
+                </div>
+              </React.Fragment>
+            )} />
+
+            <Route exact path="/member" render={props => (
+              <React.Fragment>
+                <CardList />
+              </React.Fragment>
+            )} />
+            <Route exact path="/member/:handle" component={MemberProfile} key={32}
+            />
+
+            <Route exact path="/partner" render={props => (
+              <React.Fragment>
+                <div >
+
+               {/* here will be the partner part */}
+
+                </div>
+              </React.Fragment>
+            )} />
+            <Route exact path="/coWorkingSpace" render={props => (
+              <React.Fragment>
+                <div >
+
+       {/* here will be the coWorkingSpace part */}
+
+
+                </div>
+              </React.Fragment>
+            )} />
+            <Route exact path="/consultancyAgency" render={props => (
+              <React.Fragment>
+                <div className="App-list">
+
+       {/* here will be the consultancyAgency part */}
+
+
+                </div>
+              </React.Fragment>
+            )} />
+            <Footer></Footer>
+
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Profiles/MemberProfile.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class MemberProfile extends Component {
+    state = {
+        deactivated:'', completedTaskId:'', appliedTaskId:'', skills:'', _id:'', fullName:'', webName:'', levelOfExperience:'', allRatedReco:'', averageRecoRate:'', allRatedTasks:'', dateJoined:'', __v:''
+      }
+  componentDidMount() {
+    //console.log("Hello body")
+    const { handle } = this.props.match.params
+    axios.get(`http://localhost:3333/members/${handle}`).then(res => {
+      //console.log(res);
+      this.setState({ deactivated : res.data.deactivated })
+      this.setState({ completedTaskId : res.data.completedTaskId })
+      this.setState({ appliedTaskId : res.data.appliedTaskId })
+      this.setState({ skills : res.data.skills })
+      this.setState({ _id : res.data._id })
+      this.setState({ fullName : res.data.fullName })
+      this.setState({ webName : res.data.webName })
+      this.setState({ levelOfExperience : res.data.levelOfExperience })
+      this.setState({ allRatedReco : res.data.allRatedReco })
+      this.setState({ averageRecoRate : res.data.averageRecoRate })
+      this.setState({ dateJoined : res.data.dateJoined })
+      this.setState({ __v : res.data.deactivated })
+      //console.log(this.state.users[0].fullName)
+    })
+  }
+  getMemberData(){
+ 
+      if(this.state._id!==''){
+          let memberData=[]
+          memberData.push(  <p 
+            key={this.state._id}
+            style={{
+                color:'black'
+            }}>
+        {this.state.deactivated} <br></br>{this.state.fullName} <br></br>{this.state.webName} <br></br>{this.state.levelOfExperience} <br></br>{this.state.dateJoied }</p> )
+          return memberData
+      }else{
+        return " "
+      }
+  }
+  render() {
+    return (
+  <div className="MemberProfile">
+{this.getMemberData()}
+  </div>
+
+    );
+  }
+}
+
+export default MemberProfile;
